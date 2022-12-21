b3f0631fb11379ee82947a3002a852be59ec4a75
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2022-12-21 20:33:44 +0300

    Updates

---
+++ precache-manifest.cb6e14748d2ce8ba9510c12f9e05c08a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d9c306dfe48617c85d8818c48394fe",
+    "url": "/todo-list-ts/index.html"
+  },
+  {
+    "revision": "4ee644f16f26bef74e36",
+    "url": "/todo-list-ts/static/css/main.ab1f78ef.chunk.css"
+  },
+  {
+    "revision": "f9ab966dbc426c82a218",
+    "url": "/todo-list-ts/static/js/2.7672eefa.chunk.js"
+  },
+  {
+    "revision": "7ca5d0300e49f0e7da7849add41c47f1",
+    "url": "/todo-list-ts/static/js/2.7672eefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec04326bb571eb3916b2",
+    "url": "/todo-list-ts/static/js/3.22600f93.chunk.js"
+  },
+  {
+    "revision": "4ee644f16f26bef74e36",
+    "url": "/todo-list-ts/static/js/main.5d232a96.chunk.js"
+  },
+  {
+    "revision": "9841cc8b5a8362eb60c8",
+    "url": "/todo-list-ts/static/js/runtime-main.3a905ead.js"
+  }
+]);
